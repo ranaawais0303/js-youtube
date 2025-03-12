0378e135802089ceeed5871ad24fc0594a25f932
ranaawais0303 <55804200+ranaawais0303@users.noreply.github.com>
2025-03-12 09:47:34 +0000

    data types

---
+++ 01-basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 
+    +
+3 ) //code readability should be high
+
+console.log("awais")
+
+//data types premitive 
+//number => 2 to power 53may be
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not defined yet
+// symbol=> unique
+
+//objects 
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
