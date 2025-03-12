226c02b176c0ef3c0a37d709f5d03bb4cd70d5e7
ranaawais0303 <55804200+ranaawais0303@users.noreply.github.com>
2025-03-12 07:17:47 +0000

    variables

---
+++ 01-basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+var accountEmail ="rana@gmail.com";
+let accountPassword = "212121";
+ accountCity = "Lahore";
+
+ //accountId = 23233 not allowed 
+accountEmail ="awais@gmail.com";
+accountPassword ="12341";
+accountCity ="Islamabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+ /*
+prefer not to use var
+because of issue in block scope and functional scope
+
+ */
