e02525ed57ce53343141223bd7ec3a365bda0567
ranaawais0303 <55804200+ranaawais0303@users.noreply.github.com>
2025-03-12 10:40:06 +0000

    conversions of datatype

---
+++ 01-basic/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber)
+
+//when convert into number
+//"33" => 33
+//"33abc" => NaN
+//true => 1: false=> 0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// convert into boolean
+//1=> true: 0=> false
+//"" => false
+//"something" => true
